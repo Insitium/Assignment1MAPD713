e5bef68b17930e345286ce02bda06b6fb44d7be3
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2022-09-29 11:14:50 -0400

    npm start added

---
+++ index.js
@@ -0,0 +1,13 @@
+var seneca = require('seneca')().use('product-management-require')
+seneca.use('seneca-entity');
+var products = {
+    product:"Laptop",
+    price:"201.99",
+    category:"PC"
+}
+function add_product(){
+    seneca.act({role:'products',cmd:'add',data:products},function(err,msg){
+        console.log(msg);
+    });
+}
+add_product();
